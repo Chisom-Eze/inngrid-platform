4e4c6f90845611e6cf780027b3738bf35f6fdea5
Chisom Eze <ezechisomkingsley@gmail.com>
2026-06-05 08:54:00 +0100

    Add ACM module foundation

---
+++ terraform/modules/acm/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
