fc154f5a22d10e9239eff9f69b8b49bcccd0c0b1
Chisom Eze <ezechisomkingsley@gmail.com>
2026-06-10 01:34:41 +0100

    Fix IAM role outputs and security module outputs

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -2,6 +2,6 @@ output "ecs_execution_role_arn" {
   value = aws_iam_role.ecs_execution_role.arn
 }
 
-output "ecs_task_role" {
-  value = aws_iam_role.ecs_execution_role.name
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
 }
